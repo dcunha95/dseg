e79626dd16f04fe96befa821ef64b7dd087cfe13
Daniel Araujo <64176714+danywhite11@users.noreply.github.com>
2022-03-14 22:52:11 -0300

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,11 @@
 import tensorflow as tf
 
 
+blur = tf.keras.layers.AveragePooling2D(pool_size=(5,5), padding='same')
+
 class Prep:
+    
+    
     @staticmethod
     @tf.function
     def prep_x(file_path, image_size=(512, 512)):
@@ -21,6 +25,39 @@ class Prep:
         img = tf.reshape(img, shape=(image_size[0], image_size[1], 3))
         img = tf.cast(img, dtype=tf.float32)
         return img
+
+    @staticmethod
+    @tf.function
+    def prep_y_bl(file_path, image_size=(512, 512)):
+        img = tf.io.read_file(file_path)
+        img = tf.image.decode_png(img, channels=1)
+        img = tf.stack([img == 0, img == 100, img == 255], axis=3)
+        img = tf.reshape(img, shape=(img.shape[0], img.shape[1], 3))
+        img = tf.cast(img, dtype=tf.float32)    
+        img = tf.image.resize(img, size=image_size, method='bilinear')
+        return img
+
+    @staticmethod
+    @tf.function
+    def prep_y_s(file_path, image_size=(512, 512), smoothing=2):
+        img = tf.io.read_file(file_path)
+        img = tf.image.decode_png(img, channels=1)
+        img = tf.stack([img == 0, img == 100, img == 255], axis=3)
+        img = tf.reshape(img, shape=(img.shape[0], img.shape[1], 3))
+        img = tf.cast(img, dtype=tf.float32)    
+        img = tf.image.resize(img, size=(int(image_size[0]/smoothing), int(image_size[1]/smoothing)) , method='bilinear')
+        img = tf.image.resize(img, size=image_size, method='bilinear')
+        return img
+
+    @staticmethod
+    @tf.function
+    def blur_labels(y):
+        img = tf.reshape(y, shape=(1, y.shape[0], y.shape[1], y.shape[2]))
+        img = blur(img)
+        img = tf.squeeze(img)
+        img = tf.image.resize(img, size=(y.shape[0], y.shape[1]), method='bilinear')
+        return img
+        
 
     @staticmethod
     @tf.function
